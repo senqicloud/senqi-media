8227bc7b6779b6560ac7ccdff8f077948ee0c707
codermast <codermast@qq.com>
2025-06-01 12:58:50 +0800

    :zap:更新启动类名称

--- senqi-media-server/src/main/java/com/senqicloud/senqimediaserver/SenqiMediaServerApplication.java
+++ senqi-media-server/src/main/java/com/senqicloud/senqimediaserver/SenqiMediaServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SenqiPicServerApplication {
+public class SenqiMediaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SenqiPicServerApplication.class, args);
+		SpringApplication.run(SenqiMediaServerApplication.class, args);
 	}
 
 }
